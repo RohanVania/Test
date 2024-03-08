82d1c401e5eb782ff736109cc3300ed0ab80c7fa
rohan <rohan@claclaws.com>
2024-03-08 15:28:28 -0500

    Testing

--- src/wrapper/AppWrapper.js
+++ src/wrapper/AppWrapper.js
@@ -2,6 +2,7 @@ import React from 'react'
 import NavigationDots from '../components/NavigationDots/NavigationDots'
 import SocialMedia from '../components/SocialMedia/SocialMedia'
 
+// This is Testing
 
 // Component that return another Component is Called Higher Order Component
 
