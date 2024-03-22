61fc821e06e0ed8bc9418f88599ee63ad88c5554
Rohan Vania <rohan.vania@gmail.com>
2024-03-22 07:48:33 -0400

    fixes and skills section

--- src/client.js
+++ src/client.js
@@ -1,24 +1,30 @@
 
-import {createClient} from "@sanity/client"
+import { createClient } from "@sanity/client"
 import imageUrlBuilder from "@sanity/image-url";
 
-const client=createClient({
-    projectId:'kfdyw3h2',
-    dataset:'production',
-    useCdn:true,
-    apiVersion:'2023-12-15',
+const client = createClient({
+    projectId: 'kfdyw3h2',
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: '2023-12-15',
 })
 
-const builder=imageUrlBuilder(client)
+const builder = imageUrlBuilder(client)
 
-export function urlFor(source){
+export function urlFor(source) {
     return builder.image(source)
 }
 
-export async function fetchAbouts(){
-    const query =`*[_type=="abouts"]`
-    const aboutsData=await client.fetch(query);
+export async function fetchAbouts() {
+    const query = `*[_type=="abouts"]`
+    const aboutsData = await client.fetch(query);
     return aboutsData;
+}
+
+export async function fetchSkills() {
+    const query = `*[_type=="skills"]`
+    const skillsData = await client.fetch(query);
+    return skillsData
 }
 
 
